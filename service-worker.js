d132ca4bace05289d3c7c7da7fe7eb540c32eea0
Obed Alvarado <obedalexan@gmail.com>
2024-12-06 17:44:05 -0600

    serviceworkerarreglado6

--- service-worker.js
+++ service-worker.js
@@ -16,41 +16,43 @@ const urlsToCache = [
   'img/screenshot1.png',
   'img/screenshot2.png',
   'img/sopiletras.png',
-  'offline.html'
 ];
 
-// Instalar el Service Worker
-self.addEventListener('install', (event) => {
-  console.log('Service Worker: Instalando...');
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Service Worker: Archivos cacheados');
-        return cache.addAll(urlsToCache);
-      })
-      .catch((error) => {
-        console.error('Error durante la instalación:', error);
-      })
-  );
-});
+//Funcion de instalacion
+//almacena el nombre y los archivos que van a ir guardados en cache
 
-// Activar el Service Worker
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activado');
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            console.log('Service Worker: Eliminando caché antiguo', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
-});
+self.addEventListener('install', e =>{
+  e.waitUntil( //le decimos que detenga el evento hasta que se ejecute lo siguiente
+      caches.open(CACHE_NAME)
+      .then(cache =>{
+          return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting)
+      })
+
+  )
+})
+
+self.addEventListener('activate', e =>{
+  const listaBlancaCache = [CACHE_NAME];
+
+  e.waitUntil(
+      caches.keys()
+      .then(nombresCache => {
+          return Promise.all(
+              nombresCache.map(nombresCache =>{
+                  if(listaBlancaCache.indexOf(nombresCache) === -1){
+                      return caches.delete(nombresCache)
+                  }
+              })
+          )
+      })
+      //activamos la cache actualizada
+      .then(()=> self.clients.claim())
+  )
+
+})
+
+
 self.addEventListener('fetch', e =>{
   e.respondWith(
       caches.match(e.request)
