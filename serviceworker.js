22bc146dbd4e60a37317af1bfb274f38f9f0851e
Obed Alvarado <obedalexan@gmail.com>
2024-12-07 15:50:55 -0600

    serviceworkerarreglado9

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ const urlsToCache = [
   'index.html',
   'styles.css',
   'script.js',
-  'animaciones.js', // Corregido el nombre del archivo
+  'animaciones.js',
   'img/icon_192.png',
   'img/icon_512.png',
   'img/taskifyAPP.png',
@@ -18,50 +18,41 @@ const urlsToCache = [
   'img/sopiletras.png',
 ];
 
-//Funcion de instalacion
-//almacena el nombre y los archivos que van a ir guardados en cache
-
-self.addEventListener('install', e =>{
-  e.waitUntil( //le decimos que detenga el evento hasta que se ejecute lo siguiente
-      caches.open(CACHE_NAME)
-      .then(cache =>{
-          return cache.addAll(urlsToCache)
-          .then(() => self.skipWaiting)
-      })
-
-  )
-})
-
-self.addEventListener('activate', e =>{
-  const listaBlancaCache = [CACHE_NAME];
-
+// Instalación
+self.addEventListener('install', e => {
   e.waitUntil(
-      caches.keys()
-      .then(nombresCache => {
-          return Promise.all(
-              nombresCache.map(nombresCache =>{
-                  if(listaBlancaCache.indexOf(nombresCache) === -1){
-                      return caches.delete(nombresCache)
-                  }
-              })
-          )
-      })
-      //activamos la cache actualizada
-      .then(()=> self.clients.claim())
-  )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
 
-})
+// Activación
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
 
-
-self.addEventListener('fetch', e =>{
+// Intercepción de solicitudes (fetch)
+self.addEventListener('fetch', e => {
   e.respondWith(
-      caches.match(e.request)
-      .then(res =>{
-          if(res)
-          {
-              return res
-          }
-          return fetch(e.request)
-      })
-  )
-})
+    caches.match(e.request).then(res => {
+      if (res) {
+        return res; // Recurso desde la caché
+      }
+      return fetch(e.request).catch(() => {
+        // Manejo de fallos de red
+        if (e.request.destination === 'document') {
+          return caches.match('index.html'); // Página por defecto
+        }
+      });
+    })
+  );
+});
